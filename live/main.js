b282d87d4f56286749f30fcb1423c94e29fc88f1
Shakil53 <shakilahmed.dcc@gmail.com>
2024-01-28 12:22:08 +0600

    react render

--- live/main.js
+++ live/main.js
@@ -5,9 +5,19 @@ console.log("React dom =>", ReactDOM);
 const root2 = document.getElementById('root');
 root2.innerText = 'Hello world';
 
-const App = React.createElement('h1', {style : {color: "red"}}, "Hello world3")
+const HelloWorld = () => {
+    return React.createElement("p", {}, 'Hello world')
+    
+    
+}
 
+const App = () => {
+    {
+        return React.createElement('h1', {style : {color: "red"}}, "This is a poor react app", HelloWorld())
+    }
+
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 console.log(root);
-root.render(App)
+root.render(React.createElement(App))
